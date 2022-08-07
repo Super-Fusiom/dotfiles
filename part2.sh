779c4321703edc191f829148f1e8814e6fd2c44a
Paul <paul21479@gmail.com>
2022-08-07 21:20:47 +1200

    kill me i forgot to push

--- part2.sh
+++ part2.sh
@@ -3,13 +3,12 @@
 read -rp "Hostname?" hostname
 read -rp "Username? (lowercase only)  " user
 
-echo /Arch-config/pacman.conf >> /etc/pacman.conf
+cat /Arch-config/pacman.conf >> /etc/pacman.conf
 ln -sf /usr/share/zoneinfo/NZ /etc/localtime
 nvim /etc/locale.gen
 locale-gen 
 echo "LANG=en_NZ.UTF-8" >> /etc/locale.conf
 echo "$hostname" >> /etc/hostname
-mv pacman.conf /etc/pacman.conf
 systemctl enable NetworkManager
 systemctl enable ntpd
 useradd -m "$user"
@@ -17,5 +16,5 @@ passwd "$user"
 EDITOR=nvim visudo
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-echo "sudo chown $user /Arch-config; source /Arch-config/install.sh" >> /mnt/home/"$user"/.bashrc
+echo "sudo chown $USER /Arch-config; source /Arch-config/install.sh" >> /mnt/home/"$user"/.bashrc
 exit
