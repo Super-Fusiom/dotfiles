778b3f37f2e5638de021e07d20a4fefc8909b497
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2024-01-15 18:43:29 +1300

    replace bspwm and refactor

--- part2.sh
+++ part2.sh
@@ -3,8 +3,7 @@
 read -rp "Hostname?" hostname
 read -rp "Username? (lowercase only)  " user
 
-cat /Arch-config/pacman.conf > /etc/pacman.conf
-ln -sf /usr/share/zoneinfo/NZ /etc/localtime
+ln -sf /usr/share/zoneinfo/Pacific/Auckland /etc/localtime
 nvim /etc/locale.gen
 locale-gen 
 echo "LANG=en_NZ.UTF-8" >> /etc/locale.conf
@@ -14,7 +13,7 @@ systemctl enable ntpd
 useradd -m "$user"
 passwd "$user"
 EDITOR=nvim visudo
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=LE GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 chown "$user" /Arch-config
 echo "source /Arch-config/install.sh" >> /mnt/home/"$user"/.bashrc
