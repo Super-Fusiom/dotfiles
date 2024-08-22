a4f9188256cf0b7dba3b3dc30df30a407a7d6fa3
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2024-08-22 14:16:17 +1200

    remove yt aliases

--- dotconfig/zsh/alias.zsh
+++ dotconfig/zsh/alias.zsh
@@ -1,18 +1,7 @@
 alias vim="nvim"
 alias ls="ls -alh --color"
 alias cd..="cd .."
-alias cat="bat"
+alias cat="bat" 
 
-# MPV alias for fun
-alias serbia="mpv 'https://www.youtube.com/watch?v=GURv4guxIcE'"
-alias hotel="mpv 'https://www.youtube.com/watch?v=EFH9soeufXY'"
-alias quack="mpv 'https://www.youtube.com/watch?v=q38Y5FLK63k'"
-alias payday="mpv --no-video 'https://www.youtube.com/watch?v=-2865KVcRnQ'"
-alias onepiece="mpv --no-video 'https://youtu.be/IHhNTt3oxtY'"
-alias metroid="mpv --no-video 'https://youtu.be/WLLE_PvJ5mY'"
-alias russia="mpv 'https://www.youtube.com/watch?v=y90yaLFoYoA'"
-alias weed="mpv 'https://youtu.be/n6jp2eGOTf8'"
-alias monopolyfloss="mpv 'https://www.youtube.com/watch?v=zdVeaqinEXM'"
-alias mortalkombat="mpv \"https://youtu.be/EAwWPadFsOA\""
 # Useful
-alias yt2mp3="yt-dlp -f 'ba' -x --audio-format mp3 --embed-thumbnail"
+alias yt2opus="yt-dlp -x --audio-quality 0 --audio-format opus"
